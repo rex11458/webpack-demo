11922d2d0fc99ab34781c00aebee93d9874086f0
1145811215@qq.com <1145811215@qq.com>
2019-07-26 17:20:15 +0800

    webpack 资源管理

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml'
+function component() {
+    let element = document.createElement('div')
+
+    element.innerHTML = _.join(['Hello', ' webpack'], '')
+    element.classList.add('hello')
+    
+    var myIcon = new Image();
+    myIcon.src = Icon
+    myIcon.width = 200
+    element.appendChild(myIcon)
+    console.log(Data);
+    return element
+}
+
+document.body.appendChild(component())
