8f05eb1b79402d6af8ab6cabb59f7fb7d4b9c54e
1145811215@qq.com <1145811215@qq.com>
2019-07-26 17:56:18 +0800

    webpack 开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -13,6 +20,7 @@ module.exports = {
         })
     ],
     output: {
+        publicPath:'/',
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
