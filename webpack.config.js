2d665f59bb65a4f694c9827a60873431f3ad63ec
1145811215@qq.com <1145811215@qq.com>
2019-07-30 20:20:58 +0800

    热模块替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
@@ -7,20 +8,27 @@ module.exports = {
     mode: 'development',
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: '管理输出'
-        })
+            title: '模块热替换'
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
-        publicPath:'/',
+        publicPath: '/',
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
